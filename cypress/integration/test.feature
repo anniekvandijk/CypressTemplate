0a39ca66b9c39520274cbd8ce925ede2a57e8c42
Anniek van Dijk <anniek@animundo.nl>
2018-07-09 14:10:43 +0200

    Diffrent types of step definitions

--- cypress/integration/test.feature
+++ cypress/integration/test.feature
@@ -3,5 +3,3 @@ Feature: Go to Cypress
     Scenario: Open website Cypress
         Given I go to the website "https://example.cypress.io"
         Then I see "Cypress.io: Kitchen Sink" in the title
-        
-        
--- cypress/support/step_definitions/teststeps.js
+++ cypress/support/step_definitions/teststeps.js
@@ -1,7 +1,14 @@
+/*
+The Step Definition can be a regular expression or a Cucumber expression. 
+ https://docs.cucumber.io/cucumber/step-definitions/
+*/
+
+// Example with cucumber expression.
 given('I go to the website {string}', (url) => {
     cy.visit(url)
   })
   
-then('I see {string} in the title', (title) => {
+ // Example with regulair expression.  
+then(/^I see (.*) in the title$/, (title) => {
   cy.title().should('include', title)
 })
